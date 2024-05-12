3713ef1c1845e2420561e96bf42337162b323c5a
Dale de Silva <daledesilva@gmail.com>
2024-05-12 22:44:43 +1000

    Updated tldraw version in constants

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ const manifest = require('../manifest.json');
 ////////
 
 export const PLUGIN_VERSION = manifest.version;
-export const TLDRAW_VERSION = '2.0.0-alpha.17';
+export const TLDRAW_VERSION = '2.1.0';
 export const PLUGIN_KEY = 'ddc_ink';
 export const ATTACHMENT_SUBFOLDER_NAME = 'Ink';
 export const WRITING_SUBFOLDER_NAME = 'Writing';

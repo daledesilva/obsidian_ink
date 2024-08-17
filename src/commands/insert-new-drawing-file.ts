f03e95a86296b5f06d90c6117f62de6aa310b6c2
Dale de Silva <daledesilva@gmail.com>
2024-08-17 12:13:58 +1000

    Move cursor to after embeds on insert

--- src/commands/insert-new-drawing-file.ts
+++ src/commands/insert-new-drawing-file.ts
@@ -11,8 +11,16 @@ const insertNewDrawingFile = async (plugin: InkPlugin, editor: Editor) => {
     const activeFile = plugin.app.workspace.getActiveFile();
     const fileRef = await createNewDrawingFile(plugin, activeFile);
     let embedStr = buildDrawingEmbed(fileRef.path);
+    
     activateNextEmbed();
-    editor.replaceRange( embedStr, editor.getCursor() );
+    const positionForEmbed = editor.getCursor();
+    editor.replaceRange( embedStr, positionForEmbed );
+    const positionForCursor = {...positionForEmbed};
+    
+    // Move the cursor to after the embed (Doesn't do anything if the embed is activated by default)
+    const embedLines = embedStr.split(/\r\n|\r|\n/);
+    positionForCursor.line += embedLines.length;
+    editor.setCursor(positionForCursor)
 }
 
 export default insertNewDrawingFile;
--- src/commands/insert-new-writing-file.ts
+++ src/commands/insert-new-writing-file.ts
@@ -11,8 +11,16 @@ const insertNewWritingFile = async (plugin: InkPlugin, editor: Editor) => {
     const activeFile = plugin.app.workspace.getActiveFile();
     const fileRef = await createNewWritingFile(plugin, activeFile);
     let embedStr = buildWritingEmbed(fileRef.path);
+    
     activateNextEmbed();
-    editor.replaceRange( embedStr, editor.getCursor() );
+    const positionForEmbed = editor.getCursor();
+    editor.replaceRange( embedStr, positionForEmbed );
+    const positionForCursor = {...positionForEmbed};
+    
+    // Move the cursor to after the embed (Doesn't do anything if the embed is activated by default)
+    const embedLines = embedStr.split(/\r\n|\r|\n/);
+    positionForCursor.line += embedLines.length;
+    editor.setCursor(positionForCursor)
 }
 
 export default insertNewWritingFile;

34ef4fb4df391ac430c6c936bbf3da30daadda30
Dale de Silva <daledesilva@gmail.com>
2024-09-05 22:51:09 +1000

    Updated version notice

--- src/notices/version-notices.ts
+++ src/notices/version-notices.ts
@@ -12,30 +12,22 @@ export function showVersionNotice(plugin: InkPlugin) {
     const noLastVersionTipRead = !semVer.valid(lastVersionTipRead)
     const updatedToNewerVersion = noLastVersionTipRead || semVer.gt(curVersion, lastVersionTipRead);
 
-    if(!updatedToNewerVersion) return;
-
-    switch(curVersion) {
-        case '0.2.4':   show_0_2_4_changes(plugin); break;
+    if(updatedToNewerVersion) {
+        showChanges(plugin);
     }
 }
 
 //////////
 
-function show_0_2_4_changes_maybe(plugin: InkPlugin) {
-    // Bail if it's already been shown enough times
-    if(plugin.settings.onboardingTips.welcomeTipRead) return;
-    show_0_2_4_changes(plugin);
-}
-
-function show_0_2_4_changes(plugin: InkPlugin) {
+function showChanges(plugin: InkPlugin) {
 
     const noticeBody = createInkNoticeTemplate(1,3);
-    noticeBody.createEl('h1').setText(`Changes in Ink v0.2.4`);
+    noticeBody.createEl('h1').setText(`Changes in Ink v0.3.0`);
     const listEl = noticeBody.createEl('ul');
-    listEl.createEl('li').setText(`Customise the attachment folder in Ink's settings.`);
-    listEl.createEl('li').setText(`Remove an embed section through the menu next to the lock button.`);
-    listEl.createEl('li').setText(`Additional problem solving tips added to Ink's settings.`);
-    listEl.createEl('li').setText(`Drawing embeds enabled by default.`);
+    listEl.createEl('li').setText(`Unlock multiple writing embeds at once (This is a test. Performance impact is unknown).`);
+    listEl.createEl('li').setText(`More seamless transitions between locked and unlocked writing embeds.`);
+    listEl.createEl('li').setText(`Visible grid in drawing mode (Will be optional in the future).`);
+    listEl.createEl('li').setText(`Many changes under the hood to lay groundwork for future updates and better efficiency.`);
     
     const link = noticeBody.createEl('a');
     link.setAttribute('href', 'https://youtube.com/live/nRAABxAG62o')

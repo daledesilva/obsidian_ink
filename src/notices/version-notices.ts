0e1ff8d828c86f892cf3babdfaa25bf1f1f3137e
Dale de Silva <daledesilva@gmail.com>
2024-09-11 22:41:54 +1000

    Update version notice

--- src/notices/version-notices.ts
+++ src/notices/version-notices.ts
@@ -24,13 +24,14 @@ function showChanges(plugin: InkPlugin) {
     const noticeBody = createInkNoticeTemplate(1,3);
     noticeBody.createEl('h1').setText(`Changes in Ink v0.3.0`);
     const listEl = noticeBody.createEl('ul');
-    listEl.createEl('li').setText(`Unlock multiple writing embeds at once (This is a test. Performance impact is unknown).`);
+    listEl.createEl('li').setText(`Single click unlock for embeds (This is a test, let's see how everyone likes it).`);
     listEl.createEl('li').setText(`More seamless transitions between locked and unlocked writing embeds.`);
+    listEl.createEl('li').setText(`Unlock multiple writing embeds at once (This is a test. Performance impact is unknown).`);
     listEl.createEl('li').setText(`Visible grid in drawing mode (Will be optional in the future).`);
     listEl.createEl('li').setText(`Many changes under the hood to lay groundwork for future updates and better efficiency.`);
     
     const link = noticeBody.createEl('a');
-    link.setAttribute('href', 'https://youtube.com/live/nRAABxAG62o')
+    link.setAttribute('href', 'https://youtube.com/live/iJ3i9ZYisEE')
     link.setText(`View release video`);
     // Prevent clicking link from closing notice
     link.onClickEvent( e => e.stopPropagation())

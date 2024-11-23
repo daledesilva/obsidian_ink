30663e869ec27e98587739609536a51f70b83167
Dale de Silva <daledesilva@gmail.com>
2024-11-24 00:31:36 +1100

    Refined resize icon

--- src/graphics/icons/vertical-resize-icon.tsx
+++ src/graphics/icons/vertical-resize-icon.tsx
@@ -11,9 +11,9 @@ export const VerticalResizeIcon = (props: any) => (
         height='1em'
         viewBox="0 0 24 24"
         width='1em'
-        stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" 
         {...props}
     >
-        <path d="M12 2v20"/><path d="m8 18 4 4 4-4"/><path d="m8 6 4-4 4 4"/>
+        <path d="M7.293 17.293a.999.999 0 0 1 1.414 0L12 20.586l3.293-3.293a.999.999 0 1 1 1.414 1.414l-4 4a.999.999 0 0 1-1.414 0l-4-4a.999.999 0 0 1 0-1.414ZM11.293 1.293a.999.999 0 0 1 1.414 0l4 4a.999.999 0 1 1-1.414 1.414L12 3.414 8.707 6.707a.999.999 0 1 1-1.414-1.414l4-4Z" />
+        <path d="M12 1a1 1 0 0 1 1 1v20a1 1 0 0 1-2 0V2a1 1 0 0 1 1-1Z" />
     </svg>
 );

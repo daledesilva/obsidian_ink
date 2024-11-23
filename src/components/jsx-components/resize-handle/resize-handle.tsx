ea43a08e09a3b0bb6d472a799f680da32113f2d4
Dale de Silva <daledesilva@gmail.com>
2024-11-24 00:13:58 +1100

    Prevented scrolling issues during resize on iPadOs

--- src/components/jsx-components/resize-handle/resize-handle.tsx
+++ src/components/jsx-components/resize-handle/resize-handle.tsx
@@ -13,14 +13,15 @@ interface ResizeHandleProps {
 }
 
 export const ResizeHandle: React.FC<ResizeHandleProps> = (props) => {
-	// const mousePosition = useRef<number>();
+	const lastPointerPosition = React.useRef<number>();
 
 	return <button
 		className = {classNames([
 			'ddc_ink_resize-handle',
 			'ddc_ink_vertical',
 		])}
-		onMouseDown={startResizing}
+		// onMouseDown={startResizing}
+		onPointerDown={startResizing}
 	>
 		<VerticalResizeIcon/>
 	</button>
@@ -28,15 +29,43 @@ export const ResizeHandle: React.FC<ResizeHandleProps> = (props) => {
 	// Helpers
 	//////////
 	function startResizing(e: React.MouseEvent<HTMLElement>) {
-		addEventListener("mousemove", monitorResizing);
-		addEventListener("mouseup", stopResizing);
+		document.addEventListener("mousemove", handleMouseResizing);
+		document.addEventListener("mouseup", stopResizing);
+
+		// document.addEventListener("touchstart", handleTouchResizing, { passive: false });
+		document.addEventListener("touchmove", handleTouchResizing, { passive: false });
+		document.addEventListener("touchend", stopResizing);
 	}
-	function stopResizing(e: MouseEvent) {
-		removeEventListener("mousemove", monitorResizing);
-		removeEventListener("mouseup", stopResizing);
+	function stopResizing(e: Event) {
+		document.removeEventListener("mousemove", handleMouseResizing);
+		document.removeEventListener("mouseup", stopResizing);
+
+		// document.removeEventListener("touchstart", handleMouseResizing);
+		document.removeEventListener("touchmove", handleTouchResizing);
+		document.removeEventListener("touchend", stopResizing);
+
+		delete lastPointerPosition.current;
 	}
-	function monitorResizing(e: MouseEvent) {
+	function handleMouseResizing(e: MouseEvent) {
 		props.resizeEmbed(e.movementY);
+	}
+	function handleTouchResizing(e: TouchEvent) {
+		// Prevent page scrolling while dragging
+		e.preventDefault();
+
+		// Since no scrolling occurs, obsidian gestures will kick in. This prevents them.
+		e.stopPropagation();
+
+		// Make sure there's exactly one finger
+		const touchPointer = e.changedTouches.item(0);
+		if(!touchPointer || e.changedTouches.length!==1) return;
+
+		if(lastPointerPosition.current) {
+			const vertDiff = touchPointer.pageY - lastPointerPosition.current;
+			props.resizeEmbed(vertDiff);
+		}
+
+		lastPointerPosition.current = touchPointer.pageY;
 	}
 
 };

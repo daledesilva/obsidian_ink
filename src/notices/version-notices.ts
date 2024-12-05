df3a6b8f90d1849bd6102c7bbb7f47b2b69d5217
Dale de Silva <daledesilva@gmail.com>
2024-12-05 22:32:45 +1100

    Updated version notices

--- src/notices/version-notices.ts
+++ src/notices/version-notices.ts
@@ -22,16 +22,19 @@ export function showVersionNotice(plugin: InkPlugin) {
 function showChanges(plugin: InkPlugin) {
 
     const noticeBody = createInkNoticeTemplate(1,3);
-    noticeBody.createEl('h1').setText(`Changes in Ink v0.3.0`);
+    noticeBody.createEl('h1').setText(`Changes in Ink v0.3.1`);
     const listEl = noticeBody.createEl('ul');
-    listEl.createEl('li').setText(`Single click unlock for embeds (This is a test, let's see how everyone likes it).`);
-    listEl.createEl('li').setText(`More seamless transitions between locked and unlocked writing embeds.`);
-    listEl.createEl('li').setText(`Unlock multiple writing embeds at once (This is a test. Performance impact is unknown).`);
-    listEl.createEl('li').setText(`Visible grid in drawing mode (Will be optional in the future).`);
-    listEl.createEl('li').setText(`Many changes under the hood to lay groundwork for future updates and better efficiency.`);
+    // listEl.createEl('li').setText(`Single click unlock for embeds (This is a test, let's see how everyone likes it).`);
+    // listEl.createEl('li').setText(`More seamless transitions between locked and unlocked writing embeds.`);
+    // listEl.createEl('li').setText(`Unlock multiple writing embeds at once (This is a test. Performance impact is unknown).`);
+    // listEl.createEl('li').setText(`Visible grid in drawing mode (Will be optional in the future).`);
+    // listEl.createEl('li').setText(`Many changes under the hood to lay groundwork for future updates and better efficiency.`);
+    listEl.createEl('li').setText(`Resize drawing embeds (Lock them to save the size).`);
+    listEl.createEl('li').setText(`Toggle the grid on and off from the dropdown.`);
+    listEl.createEl('li').setText(`Insert commands now have icons.`);
     
     const link = noticeBody.createEl('a');
-    link.setAttribute('href', 'https://youtube.com/live/iJ3i9ZYisEE')
+    link.setAttribute('href', 'https://www.youtube.com/live/gLserf5LLD0?si=mS95cP0fK0d0bryo')
     link.setText(`View release video`);
     // Prevent clicking link from closing notice
     link.onClickEvent( e => e.stopPropagation())

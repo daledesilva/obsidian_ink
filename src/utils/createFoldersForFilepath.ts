1dc327f721f81aa2953c11a06562628150e97af5
Dale de Silva <daledesilva@gmail.com>
2025-01-02 17:01:42 +1100

    Fix for most inner folder not being visibly created on iPadOS

--- src/utils/createFoldersForFilepath.ts
+++ src/utils/createFoldersForFilepath.ts
@@ -1,15 +1,30 @@
 import InkPlugin from "src/main";
+import { debug, error } from "./log-to-console";
 
+///////////////////////
+///////////////////////
 
-export const createFoldersForFilepath = async (plugin: InkPlugin, path: string): Promise<void> => {
-
+export const createFoldersForFilepath = async (plugin: InkPlugin, filePath: string): Promise<void> => {
+    const splitPath = filePath.split('/');
+    
     // Remove the filename at the end
-    const folders = path.split('/');
-    folders.pop();
+    splitPath.pop();
 
-    try {
-        await plugin.app.vault.createFolder(folders.join('/'));
-    } catch (e) {
-        // console.log(e);
-    }
+    let prevPath: string[] = [];
+    for(let i=0; i<splitPath.length; i++) {
+        const folderName = splitPath[i];
+        const cascadePath = prevPath.length > 0 ? `${prevPath.join('/')}/${folderName}` : folderName;
+
+        try {
+            if (!plugin.app.vault.getAbstractFileByPath(cascadePath)) {
+                await plugin.app.vault.createFolder(cascadePath);
+                debug(`Created path: ${cascadePath}`);
+            }
+        } catch (e) {
+            error(`Couldn't create attachment folder for ${cascadePath}`, e);
+        }
+
+        prevPath.push(folderName);
+    };
+    
 };
